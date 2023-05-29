0bb8f28a7112efd57fe82ab56350910af019adcb
Rohit Balage <thatcomputerboy26@gmail.com>
2023-05-28 23:21:16 -0400

    2- taking input from the user is added

---
+++ 2- taking input from the user in cpp.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+This is problem statement;
+
+taking input from the user
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout <<"you have entered: "<<n;
+    return 0;
+}
