7e5aa78440f467c5aba1abb57e3bf65d4d497a6a
Rohit Balage <thatcomputerboy26@gmail.com>
2023-05-28 23:26:38 -0400

    3- input string from user

---
+++ 3- input string from user in cpp.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+This is problem statement;
+
+taking input from the user
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    
+    cin >> name;
+    
+    cout <<"Your name is: "<<name;
+}
