7db46065973095d3bcc6616e885347e38408ea9b
Rohit Balage <thatcomputerboy26@gmail.com>
2023-07-12 23:25:16 -0400

    17- cpp program to print pattern till n

---
+++ 17- cpp program to print pattern till n.cpp
@@ -0,0 +1,31 @@
+/*  printing pattern 
+
+1 
+12 
+123
+1234 
+12345
+123456
+till n
+
+*/
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+ 
+ int n, i,j;
+ 
+ cout << "enter your number";
+ cin >>n;
+
+for (int i = 1; i <= n;i++) {
+        for (int j = 1; j <= i; j++) {
+            std::cout << j;
+        }
+        cout << endl;
+    }
+}
