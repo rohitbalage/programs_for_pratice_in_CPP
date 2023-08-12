18c497fc0a9598c2e9b715797a8f608147bf9ba9
Rohit Balage <thatcomputerboy26@gmail.com>
2023-08-12 00:41:15 -0400

    24- pattern in cpp 1-23-345-4567.cpp

---
+++ .vscode/24- pattern in cpp 1-23-345-5678.cpp
@@ -0,0 +1,36 @@
+/* pattern to print
+1 
+23
+345
+4567
+56789
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int n;
+    cin>>n;
+    
+    int i=1;
+     int val =1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            cout <<val;
+            j++;
+            val++;
+            
+        }
+        i++;
+        
+        cout<<endl;
+    }
+}
